e65d095374cbcf754a50b56e62917ed7fd43e790
jason <jduong89@gmail.com>
2022-04-13 00:10:35 -0700

    adds index create and show

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,59 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      places: [],
+      newPlaceParams: {},
+      errors: [],
+      place: {},
+      currentPlace: {},
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/places").then((response) => {
+      this.places = response.data;
+      console.log(this.places);
+    });
+  },
+  methods: {
+    createPlace: function () {
+      axios
+        .post("http://localhost:3000/places", this.newPlaceParams)
+        .then((response) => {
+          console.log("Success", response.data);
+          this.places.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showPlace: function (place) {
+      console.log(place);
+      this.currentPlace = place;
+      document.querySelector("#place-details").showModal();
+    },
   },
 };
 </script>
+
+<template>
+  <h1>Places</h1>
+  <p>
+    Location:
+    <input type="text" v-model="newPlaceParams.name" />
+    Address:
+    <input type="text" v-model="newPlaceParams.address" />
+  </p>
+  <button v-on:click="createPlace()">Create</button>
+  <div v-for="place in places" :key="place">
+    <p>{{ place.name }}</p>
+    <button v-on:click="showPlace(place)">Address</button>
+  </div>
+  <dialog id="place-details">
+    <form method="dialog">
+      <h1>{{ currentPlace.name }}</h1>
+      <p>{{ currentPlace.address }}</p>
+      <button>Close</button>
+    </form>
+  </dialog>
+</template>
